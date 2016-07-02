f73335e36062c68d8e8595d0d59cc0d9aa849a86
GPAMC <synyster_gc@hotmail.com>
2016-07-02 14:52:31 +0100

    Initial Commit

---
+++ TestFrameworkTwo/TestFrameworkTwo.h
@@ -0,0 +1,19 @@
+//
+//  TestFrameworkTwo.h
+//  TestFrameworkTwo
+//
+//  Created by Graham Connolly on 02/07/2016.
+//  Copyright © 2016 Graham Connolly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFrameworkTwo.
+FOUNDATION_EXPORT double TestFrameworkTwoVersionNumber;
+
+//! Project version string for TestFrameworkTwo.
+FOUNDATION_EXPORT const unsigned char TestFrameworkTwoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFrameworkTwo/PublicHeader.h>
+
+
